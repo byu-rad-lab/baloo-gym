0aa3ab489b68d4b3ae54521a241fdfde91463939
Curtis <curtiscarljohnson@gmail.com>
2024-09-06 16:44:17 -0600

    simplifying rewards.

--- wrappers/three_part_reward_wrapper.py
+++ wrappers/three_part_reward_wrapper.py
@@ -51,7 +51,7 @@ class ThreePartRewardWrapper(gym.Wrapper):
         reward = R_approach + R_sensor + R_grasp + R_body
         """
 
-        reward = 0
+        reward = -1
 
         # #add joint centering penalty on q^2
         # right_j0 = get_joint_angles(self.env.unwrapped.model,
@@ -67,69 +67,70 @@ class ThreePartRewardWrapper(gym.Wrapper):
         # left_j2 = get_joint_angles(self.env.unwrapped.model,
         #                            self.env.unwrapped.data, 'left', 2)
 
-        # joint_penalty = -np.linalg.norm(right_j0)**2 - np.linalg.norm(
-        #     right_j1)**2 - np.linalg.norm(right_j2)**2 - np.linalg.norm(
-        #         left_j0)**2 - np.linalg.norm(left_j1)**2 - np.linalg.norm(
-        #             left_j2)**2
+        # joint_penalty = -(
+        #     np.linalg.norm(right_j0)**2 + np.linalg.norm(right_j1)**2 +
+        #     np.linalg.norm(right_j2)**2 + np.linalg.norm(left_j0)**2 +
+        #     np.linalg.norm(left_j1)**2 + np.linalg.norm(left_j2)**2)
 
-        #penalize being far from the box
+        # reward += joint_penalty
+
+        #penalize chest being far from the box
         chest_pos = self.env.unwrapped.data.geom('chest').xpos
         box_pos = get_box_position(self.env.unwrapped.model,
                                    self.env.unwrapped.data)
         reward += -1 * np.linalg.norm(chest_pos - box_pos)
 
-        #penalize touching object until we are ready to grasp
-        if self.state == 'approach':
-            #penalize touching object until we are ready to grasp
-            box_touched = detect_box_touch(self.env.unwrapped.model,
-                                           self.env.unwrapped.data)
-            if box_touched:
-                reward += -10
+        # #penalize touching object until we are ready to grasp
+        # if self.state == 'approach':
+        #     #penalize touching object until we are ready to grasp
+        #     box_touched = detect_box_touch(self.env.unwrapped.model,
+        #                                    self.env.unwrapped.data)
+        #     if box_touched:
+        #         reward += -1
 
-            #if height of chest is within a certain range of manipuland centroid, then we are ready to grasp
-            if abs(chest_pos[2] - box_pos[2]) < 0.25:
-                self.state = 'grasp'
+        #     #if height of chest is within a certain range of manipuland centroid, then we are ready to grasp
+        #     if abs(chest_pos[2] - box_pos[2]) < 0.25:
+        #         self.state = 'grasp'
 
-                #change box color to green
-                self.env.unwrapped.model.geom('box').rgba = [0, 1, 0, 1]
+        #         #change box color to green
+        #         self.env.unwrapped.model.geom('box').rgba = [0, 1, 0, 1]
 
-        elif self.state == 'grasp':
-            #reward = r_sensor + r_grasp
-            L_T0 = get_tactile_image(self.env.unwrapped.model,
-                                     self.env.unwrapped.data, 'left', 0)
-            L_T1 = get_tactile_image(self.env.unwrapped.model,
-                                     self.env.unwrapped.data, 'left', 1)
-            R_T0 = get_tactile_image(self.env.unwrapped.model,
-                                     self.env.unwrapped.data, 'right', 0)
-            R_T1 = get_tactile_image(self.env.unwrapped.model,
-                                     self.env.unwrapped.data, 'right', 1)
-            C_T = get_tactile_image(self.env.unwrapped.model,
-                                    self.env.unwrapped.data, 'chest', None)
+        # elif self.state == 'grasp':
+        #reward = r_sensor + r_grasp
+        L_T0 = get_tactile_image(self.env.unwrapped.model,
+                                 self.env.unwrapped.data, 'left', 0)
+        L_T1 = get_tactile_image(self.env.unwrapped.model,
+                                 self.env.unwrapped.data, 'left', 1)
+        R_T0 = get_tactile_image(self.env.unwrapped.model,
+                                 self.env.unwrapped.data, 'right', 0)
+        R_T1 = get_tactile_image(self.env.unwrapped.model,
+                                 self.env.unwrapped.data, 'right', 1)
+        C_T = get_tactile_image(self.env.unwrapped.model,
+                                self.env.unwrapped.data, 'chest', None)
 
-            #encourage grasping with sensor areas with average frobenius norm
-            r_sensor = (np.log(1 + np.linalg.norm(L_T0, 'fro')) +
-                        np.log(1 + np.linalg.norm(L_T1, 'fro')) +
-                        np.log(1 + np.linalg.norm(R_T0, 'fro')) +
-                        np.log(1 + np.linalg.norm(R_T1, 'fro')) +
-                        np.log(1 + np.linalg.norm(C_T, 'fro')))
-            reward += r_sensor / 5
+        #encourage grasping with sensor areas with average frobenius norm
+        r_sensor = (np.log(1 + np.linalg.norm(L_T0, 'fro')) +
+                    np.log(1 + np.linalg.norm(L_T1, 'fro')) +
+                    np.log(1 + np.linalg.norm(R_T0, 'fro')) +
+                    np.log(1 + np.linalg.norm(R_T1, 'fro')) +
+                    np.log(1 + np.linalg.norm(C_T, 'fro')))
+        reward += r_sensor / 5
 
-            # #penalize large differences between the two arms.
-            # r_grasp = -np.linalg.norm(L_T0 - R_T0, 'fro') + np.linalg.norm(
-            #     L_T1 - R_T1, 'fro')
+        # #penalize large differences between the two arms.
+        # r_grasp = -np.linalg.norm(L_T0 - R_T0, 'fro') + np.linalg.norm(
+        #     L_T1 - R_T1, 'fro')
 
-            #reward for box moving upwards, implying that it was somehow lifted up. This also penalizes box falling down.
-            box_pos = get_box_position(self.env.unwrapped.model,
-                                       self.env.unwrapped.data)
-            r_box = box_pos[2] - self.box_pos[2]
-            reward += 10 * r_box
+        #reward for box moving upwards, implying that it was somehow lifted up. This also penalizes box falling down.
+        box_pos = get_box_position(self.env.unwrapped.model,
+                                   self.env.unwrapped.data)
+        r_box = box_pos[2] - self.box_pos[2]
+        reward += 10 * r_box
 
-            #penalize equally for large changes in the box orientation
-            box_quat = R.from_quat(
-                get_box_quat(self.env.unwrapped.model,
-                             self.env.unwrapped.data))
+        #penalize equally for large changes in the box orientation
+        box_quat = R.from_quat(
+            get_box_quat(self.env.unwrapped.model, self.env.unwrapped.data))
 
-            diff = box_quat.inv() * self.box_quat
-            reward += -10 * np.linalg.norm(diff.magnitude())
+        diff = box_quat.inv() * self.box_quat
+        reward += -10 * np.linalg.norm(diff.magnitude())
 
         return reward
