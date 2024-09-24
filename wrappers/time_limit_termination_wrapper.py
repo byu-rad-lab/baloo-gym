89ecf7430264befa6d56bfa5ceca9301a2d7184f
Curtis <curtiscarljohnson@gmail.com>
2024-09-24 12:40:00 -0600

    using step count instead to avoid numerical precision errors.

--- wrappers/time_limit_termination_wrapper.py
+++ wrappers/time_limit_termination_wrapper.py
@@ -8,20 +8,28 @@ class TimeLimitTerminationWrapper(gym.Wrapper):
     """
     Now this class can just overrides the calculate_reward() method to return a force based reward. 
     """
-    def __init__(self, env, time_limit_sec):
+    def __init__(self, env, time_limit_sec, control_timestep):
         """Constructor for the Reward wrapper."""
         super().__init__(env)
         self.time_limit_sec = time_limit_sec
+        self.step_count = 0
+        self.control_timestep = control_timestep
 
     def step(self, action):
         """Step function that calls the parent step function and then calculates the reward."""
+
         # call baloo_base step function
         observation, reward, terminated, truncated, info = self.env.step(
             action)
 
-        sim_time = self.env.unwrapped.data.time
+        self.step_count += 1
+        print(self.step_count)
 
-        if sim_time >= self.time_limit_sec:
+        if self.step_count == int(self.time_limit_sec / self.control_timestep):
             terminated = True
 
         return observation, reward, terminated, truncated, info
+
+    def reset(self):
+        self.step_count = 0
+        return self.env.reset()
