708a0b2b14ded7be17c69f5fd2012ea4265d655f
Curtis <curtiscarljohnson@gmail.com>
2024-10-10 15:26:19 -0600

    adding plots to visualize performance.

--- hugger_policy_rollout.py
+++ hugger_policy_rollout.py
@@ -1,5 +1,7 @@
 from baloo_gym.policies.open_loop_hugger import OpenLoopHuggerPolicy
 from baloo_gym.utils.helpers import build_env, record_rollout, make_movie
+import matplotlib.pyplot as plt
+import numpy as np
 
 #just pick a config right now. The config is only really used for the environment name.
 
@@ -23,3 +25,45 @@ model = OpenLoopHuggerPolicy(N=100)
 frames, rewards, actions, observations = record_rollout(env, model)
 
 make_movie(frames, "test.mp4", fps=1 / config["ctrl_timestep"])
+
+plt.plot(rewards)
+plt.xlabel("Timesteps")
+plt.ylabel("Rewards")
+plt.savefig("rewards.png", dpi=300)
+
+fig, axs = plt.subplots(
+    env.action_space.shape[0],
+    1,
+    sharex=True,
+    figsize=(10, 30),
+)
+for i in range(env.action_space.shape[0]):
+    axs[i].plot(np.array(actions)[:, i])
+    axs[i].set_ylabel(f"a{i}")
+
+axs[-1].set_xlabel("Timesteps")
+plt.savefig("actions.png", dpi=300, bbox_inches='tight')
+
+# make histogram of actions
+fig, axs = plt.subplots(
+    env.action_space.shape[0],
+    1,
+    figsize=(10, 30),
+)
+for i in range(env.action_space.shape[0]):
+    axs[i].hist(np.array(actions)[:, i], bins=100)
+    axs[i].set_ylabel(f"a{i}")
+
+axs[-1].set_xlabel("Action Value")
+plt.savefig("actions_hist.png", dpi=300, bbox_inches='tight')
+
+fig, axs = plt.subplots(env.observation_space.shape[0],
+                        1,
+                        sharex=True,
+                        figsize=(10, 40))
+for i in range(env.observation_space.shape[0]):
+    axs[i].plot(np.array(observations)[:, i])
+    axs[i].set_ylabel(f"o{i}")
+
+axs[-1].set_xlabel("Timesteps")
+plt.savefig("observations.png", dpi=300, bbox_inches='tight')
