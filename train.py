0d0f3c0f9b3674f125cb0166b2c6a9453b237aef
Curtis <curtiscarljohnson@gmail.com>
2025-03-14 10:33:35 -0600

    squash output and a bit of entropy

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ def train(args):
     policy_kwargs = dict(
         net_arch=[128, 64],
         use_expln=True,
-        # squash_output=True,
+        squash_output=True,
         log_std_init=-3,
     )
     model = PPO(
@@ -149,7 +149,7 @@ def train(args):
         sde_sample_freq=2 / config["ctrl_timestep"],
         batch_size=128,
         learning_rate=linear_schedule(5e-4, 1e-6),
-        ent_coef=.00,
+        ent_coef=.005,
         verbose=2,
         tensorboard_log=f"new_experiments/{run_folder}/tensorboard_logs",
         device="auto",
